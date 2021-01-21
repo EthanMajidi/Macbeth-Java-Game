675c0f0033b56df62d5e1a1ce7cf9328b2fdf2b9
Ethan Majidi <ethan.majidi@gmail.com>
2021-01-21 14:07:22 -0500

    project first commit

---
+++ core/src/com/ethanmajidi/javagame/Screens/Controller.java
@@ -0,0 +1,20 @@
+package com.ethanmajidi.javagame.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+
+/**
+ * Created by EthanMajidi on 2018-01-27.
+ */
+
+public class Controller {
+}
